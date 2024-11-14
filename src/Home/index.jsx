8207c947af531b04cd9b35e3b7ad57807d29a2db
Evan-cell <malcomchege0582@gmail.com>
2024-11-14 12:25:18 +0300

    database

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,9 +1,12 @@
+import Header from '@/components/custom/Header'
+import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
 
 function Home() {
   return (
     <div>
-      Home
+      <Header/>
+      Landing screen
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,28 +6,35 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SignInPage from './auth/sign-in'
 import Home from './Home'
 import Dashboard from './dashboard'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 const router = createBrowserRouter([
   {
+    path:'/',
+    element:<Home/>
+  },
+  {
     element:<App/>,
     children:[
-      { path:'/',
-         element:<Home/>
-      },
       {
         path:'/dashboard',
         element:<Dashboard/>
-      }
-]
+      },
+    ]
   },
   {
     path:'/auth/sign-in',
-    element: <SignInPage/>
-  }
+    element:<SignInPage/>
+  },
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <RouterProvider router={router}/>
+    </ClerkProvider>
   </StrictMode>,
 )
